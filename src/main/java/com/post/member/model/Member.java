1db442441a6f980d0ec11289b3916565638b9a4b
양길완 <novot@nate.com>
2018-10-27 14:00:47 +0900

    Member 추가

---
+++ src/main/java/com/post/member/model/Member.java
@@ -0,0 +1,16 @@
+package com.post.member.model;
+
+import java.time.LocalDateTime;
+
+public class Member {
+    private Long memberNo;
+    private String id;
+    private String password;
+    private String name;
+    private String cellPhone;
+    private String phone;
+    private String email;
+    private String creator;
+    private LocalDateTime createDt;
+
+}
